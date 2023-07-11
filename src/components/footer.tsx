441df46e7a323d68fdc278340fcf71e71361be1d
Joe Trippier <joe.trippier@coop.co.uk>
2023-07-11 08:52:35 +0100

    Add basic content to main page

---
+++ src/components/footer.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+const Footer = () => {
+  return <footer className="container mx-auto px-8"></footer>;
+};
+
+export default Footer;
---
+++ src/components/header.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+
+const Header = () => {
+  return <header className="container mx-auto px-8"></header>;
+};
+
+export default Header;
